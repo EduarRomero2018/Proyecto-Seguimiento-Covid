1b4089f26af633ca9e092e84bbd4529c55f530ac
DanielZC <zabaletacastellar@gmail.com>
2020-06-27 14:51:03 -0500

    se añadio datatable

--- views/listar_pacientes_view.php
+++ views/listar_pacientes_view.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+    <meta charset="UTF-8">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
@@ -12,129 +13,65 @@
 </head>
 
 <body>
+    <div class="linea1"></div>
+    <div class="linea2"></div>
+    <div class="linea3"></div>
 
     <div class="container-fluid">
         <?php if (isset($res) != '') : ?>
-        <div class="card shadow mt-5">
-            <div class class="col text-left">
-                <a href="index.php">
-
-                    <button type="button" class="btn btn-outline-secondary btn-lg"> <i
-                            class="fas fa-chevron-left"></i></button>
-                </a>
-            </div>
-            <div class="card-body">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <h3>Paciente registrados: <?= $query->rowCount() ?></h3>
-                        </div>
-
-                        <div class="col-md-4 mb-3">
-                            <div class="input-group">
-                              <div class="input-group-prepend">
-                                <input type="text" class="form-control" id="buscar"  placeholder="Buscar Paciente"
-                                aria-describedby="inputGroupPrepend21" onkeyup="busqueda();">
-                                <a href="listar_pacientes.php">
-                                    <span type="submi" class="input-group-text" id="inputGroupPrepend21">Borrar</span>
-                                </a>
-                            </div>
-
-
-
-                              </div>
-                          </div>
-                        </div>
+            <div class="card shadow mt-5">
+                <div class="card-body">
+                    <div class class="col text-left">
+                        <a href="index.php">
+                            <button type="button" class="btn btn-outline-secondary btn-lg"> <i class="fas fa-chevron-left"></i></button>
+                        </a>
                     </div>
-                        </div>
-
-                <div class="container">
-                    <div class="linea1"></div>
-                    <div class="linea2"></div>
-                    <div class="linea3"></div>
-                </div>
-                <?php $cant_pag = round($cantidad / $fin);?>
-
-                <table id="search" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
-                    <thead>
-                        <tr>
-                            <th class="th-sm">Registro
-                            </th>
-                            <th class="th-sm">Nombre y Apellido
-                            </th>
-                            <!--<th class="th-sm">Tipo Documento
-                            </th>-->
-                            <th class="th-sm">Identificacion
-                            </th>
-                            <th class="th-sm">Edad
-                            </th>
-                            <!--<th class="th-sm">Sexo
-                            </th>-->
-                            <!--<th class="th-sm">Dirección
-                            </th>-->
-                            <!--<th class="th-sm">Correo
-                            </th>-->
-                            <th class="th-sm">Telefono
-                            </th>
-                            <!--<th class="th-sm">Fecha Entrega laboratorio
-                            </th>-->
-                            <th class="th-sm">Fecha de Resultado
-                            </th>
-                            <th class="th-sm">Resultados
-                            </th>
-                        </tr>
-                    </thead>
-
-                    <tbody id="listar_pacientes">
-                        <?php $i = 1;
-                            foreach ($res as $results) : ?>
-                        <tr data-toggle="popover-hover" data-content="">
-                            <td><?= $i ?></td>
-                            <td> <?= $results->Nombre_Completo?></td>
-                            <td><?= $results->Identificacion?></td>
-                            <!--<td><?= $results->numero_documento?></td>-->
-                            <td><?= $results->Edad?></td>
-                            <!--<td><?= $results->sexo?></td>-->
-                            <!--<td><?= $results->barrio?></td>-->
-                            <!--<td><?= $results->correo?></td>-->
-                            <td><?= $results->telefono?></td>
-                            <!--<td><?= $results->fecha_entrega_lab?></td>-->
-                            <td><?= $results->fecha_resultado?></td>
-                            <td><?= $results->resultado?></td>
-
-                        </tr>
-                        <?php $i = $i + 1;
-                            endforeach; ?>
-                    </tbody>
-                </table>
-                <div id="paginacion" class="d-flex justify-content-center mt-3">
-                    <nav aria-label="Page navigation example">
-                        <ul class="pagination">
-                            <?php $p = isset($_REQUEST['p']) ? $_REQUEST['p'] : 0 ?>
-                            <li class="page-item <?= $p > 1 ? : 'disabled' ?>">
-                                <a class="page-link"
-                                    href="listar_pacientes.php?p=<?= isset($_REQUEST['p']) ? $_REQUEST['p'] - 1: 1 ?>"
-                                    aria-label="Previous">
-                                    <span aria-hidden="true">&laquo; Anterior</span>
-                                </a>
-                            </li>
-                            <?php for ($i = 1; $i <= $cant_pag; $i++): ?>
-                            <li class="page-item <?= isset($_REQUEST['p']) && $_REQUEST['p'] == $i ? 'active':'' ?>"><a
-                                    class="page-link" href="listar_pacientes.php?p=<?= $i ?>"><?= $i ?></a></li>
-                            <?php endfor ?>
-                            <?php $n = isset($_REQUEST['p']) ? $_REQUEST['p'] + 1: 0;?>
-                            <li class="page-item <?= $n < $i ? : 'disabled' ?>">
-                                <a class="page-link"
-                                    href="listar_pacientes.php?p=<?= isset($_REQUEST['p'])?$_REQUEST['p'] + 1: 0 + 1 ?>"
-                                    aria-label="Next">
-                                    <span aria-hidden="true">Siguiente &raquo;</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
+                    <div class="container">
+                    </div>
+                    <div class="table-wrapper-scroll-y my-custom-scrollbar table-hover">
+                        <table id="dtBasicExample" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
+                            <thead>
+                                <tr class="text-right">
+                                    <th class="text-center th-sm"># Registro<i class="text-left fas fa-sort ml-1"></i></th>
+                                    <th class="text-center th-sm">Nombre paciente<i class="text-left fas fa-sort ml-1"></i></th>
+                                    <th class="text-center th-sm">Edad<i class="text-left fas fa-sort ml-1"></i></th>
+                                    <th class="text-center th-sm">Identificacion<i class="text-left fas fa-sort ml-1"></i></th>
+                                    <th class="text-center th-sm">Telefono<i class="text-left fas fa-sort ml-1"></i></th>
+                                    <th class="text-center th-sm">Fecha Del Resultado<i class="text-left fas fa-sort ml-1"></i></th>
+                                    <th class="text-center th-sm">Resultado<i class="text-left fas fa-sort ml-1"></i></th>
+                                </tr>
+                            </thead>
+                            <tbody id="tbl-llamadas">
+                                <?php
+                                $i = 1;
+                                foreach ($res as $key) : ?>
+                                    <tr>
+                                        <td class="text-center"><?= $i ?></td>
+                                        <td><?= $key->Nombre_Completo ?></td>
+                                        <td class="text-center"><?= $key->Edad ?></td>
+                                        <td><?= $key->Identificacion ?></td>
+                                        <td><?= $key->telefono ?></td>
+                                        <td class="text-center"><?= $key->fecha_resultado ?></td>
+                                        <td><?= $key->resultado ?></td>
+                                    </tr>
+                                <?php $i++;
+                                endforeach; ?>
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th class="text-center"># Registro</th>
+                                    <th class="text-center">Nombre paciente</th>
+                                    <th class="text-center">Edad</th>
+                                    <th class="text-center">Identificacion</th>
+                                    <th class="text-center">Telefono</th>
+                                    <th class="text-center">Fecha Del Resultado</th>
+                                    <th class="text-center">Resultado</th>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
         <?php endif; ?>
     </div>
 </body>
@@ -142,61 +79,8 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js">
 </script>
+<script src="js/addons/datatables.min.js"></script>
+<script src="js/tables.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.0/js/mdb.min.js"></script>
 
-<script>
-    $(document).ready(function () {
-        $('[data-toggle="popover-hover"]').popover({
-            html:true,
-            trigger: 'hover',
-            placement: 'top'
-        });
-        console.log('ajax');
-
-        $('#buscar').on('keyup', function () {
-            let consulta = $('#buscar').val()
-            $('#paginacion').attr('hidden',true)
-
-            $.ajax({
-                url: "listar_pacientes.php",
-                type: "POST",
-                data: {
-                    consulta
-                },
-                success: function (response) {
-                    let resultados = JSON.parse(response)
-                    switch (resultados) {
-
-                        default:
-                            let plantilla = ''
-                            let i = 1
-
-                            resultados.forEach(element => {
-                                plantilla += `
-                                        <tr>
-                                            <td>${i}</td>
-                                            <td>${element.Nombre_Completo}</td>
-                                            <td>${element.Identificacion}</td>
-                                            <td>${element.Edad}</td>
-                                            <td>${element.telefono}</td>
-                                            <td>${element.fecha_resultado}</td>
-                                            <td>${element.resultado}</td>
-
-                                        </tr>
-                                    `
-                                i = i + 1
-                            });
-                            $('#listar_pacientes').html(plantilla)
-                            console.log(resultados);
-                            break;
-                    }
-
-                }
-            });
-        })
-    })
-</script>
-
 </html>
-
-
--- views/soporte_resultado_view.php
+++ views/soporte_resultado_view.php
@@ -1,26 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Soporte Resultado</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.29.0/sweetalert2.all.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/stylos_formulario.css">
+    <link rel="stylesheet" href="css/mdb.min.css">
     <script src="js/jquery.js"></script>
     <link rel="stylesheet" href="css/stylos_formulario.css">
 </head>
+
 <body>
+    <div class="linea1"></div>
+    <div class="linea2"></div>
+    <div class="linea3"></div>
     <div class="container-fluid">
-        <div class="linea1"></div>
-        <div class="linea2"></div>
-        <div class="linea3"></div>
         <div class="d-flex justify-content-center mb-5 mt-5">
             <div class="card col-md-6 shadow">
                 <div class="card-body">
-                    <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" enctype="multipart/form-data"
-                        method="post">
+                    <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" enctype="multipart/form-data" method="post">
                         <div class="form-group">
                             <label>Ingres el Documento de Paciente</label>
                             <input class="form-control" type="text" name="documento">
@@ -42,74 +45,93 @@
                             <button type="button" class="btn btn-outline-secondary btn-lg">Regresar</button>
                         </a>
                     </form>
-                    <?php if(!empty($exito)):?>
-                <script>
-                  swal({
-                    type: 'success',
-                    title: "Datos Guardados Exitosamente",
-                    text: "<?php echo $exito; ?>",
-                    button: "Aceptar",
-                    icon: "error", button: "Aceptar",
-                    timer: 7000,
-                    animation: false,
-                    customClass: 'animated heartBeat'
-                  })
-                </script>
-                <?php endif; ?>
+                    <?php if (!empty($exito)) : ?>
+                        <script>
+                            swal({
+                                type: 'success',
+                                title: "Datos Guardados Exitosamente",
+                                text: "<?php echo $exito; ?>",
+                                button: "Aceptar",
+                                icon: "error",
+                                button: "Aceptar",
+                                timer: 7000,
+                                animation: false,
+                                customClass: 'animated heartBeat'
+                            })
+                        </script>
+                    <?php endif; ?>
 
-                <?php if(!empty($errores)):?>
-    <script>
-      swal({
-        type: 'error',
-        title: "ERROR",
-        text: "<?php echo $errores; ?>",
-        button: "Aceptar",
-        icon: "error", button: "Aceptar",
-        timer: 7000,
-        animation: false,
-        customClass: 'animated heartBeat'
-      })
-    </script>
-    <?php endif; ?>
+                    <?php if (!empty($errores)) : ?>
+                        <script>
+                            swal({
+                                type: 'error',
+                                title: "ERROR",
+                                text: "<?php echo $errores; ?>",
+                                button: "Aceptar",
+                                icon: "error",
+                                button: "Aceptar",
+                                timer: 7000,
+                                animation: false,
+                                customClass: 'animated heartBeat'
+                            })
+                        </script>
+                    <?php endif; ?>
                 </div>
 
             </div>
         </div>
         <?php if (isset($result) && $result != '') : ?>
-        <h3>Historial de soportes de resultado del paciente: <?= ucwords($Nombre_Completo) ?></h3>
-        <table class="table table-hover">
-            <thead>
-                <tr>
-                    <th># Registro</th>
-                    <th>Documento</th>
-                    <th>Fecha Registro</th>
-                    <th>Url</th>
-                    <th>Descargar Archivo</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php $i = 1;
-                    foreach ($result as $row) : ?>
-                <tr>
-                    <td><?= $i ?></td>
-                    <td id="documento"><?= $documento ?></td>
-                    <td><?=$row->fecha_resgistro?></td>
-                    <td><small class="text-muted"><?= base64_encode($row->soporte_resultado) ?></small></td>
-                    <td><a name="descargar" id="<?=$row->soporte_resultado?>" href="">Descargar</a></td>
-                </tr>
-                <?php $i = $i + 1; endforeach; ?>
-            </tbody>
-        </table>
-        <legend hidden id="nombre"><?=$Nombre_Completo?></legend>
-        <?php endif; ?>
+            <div class="card shadow">
+                <div class="card-body">
+                    <h3>Historial de soportes de resultado del paciente: <?= ucwords($Nombre_Completo) ?></h3>
+                    <div class="table-wrapper-scroll-y my-custom-scrollbar table-hover">
+                        <table id="dtBasicExample" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
+                        <thead>
+                                <tr class="text-right">
+                                    <th class="text-center th-sm"># Registro<i class="text-left fas fa-sort ml-1"></i></th>
+                                    <th class="text-center th-sm">Documento</th>
+                                    <th class="text-center th-sm">Fecha Registro<i class="text-left fas fa-sort ml-1"></i></th>
+                                    <th class="text-center th-sm">Url</th>
+                                    <th class="text-center th-sm">Descargar Archivo</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $i = 1;
+                                foreach ($result as $row) : ?>
+                                    <tr>
+                                        <td class="text-center"><?= $i ?></td>
+                                        <td id="documento"><?= $documento ?></td>
+                                        <td><?= $row->Fecha_registro ?></td>
+                                        <td><small class="text-muted"><?= base64_encode($row->soporte_resultado) ?></small></td>
+                                        <td class="text-center"><a name="descargar" id="<?= $row->soporte_resultado ?>" href="" class="btn btn-info btn-sm"><i style="font-size: 20px;" class="text-white fas fa-file-download"></i></a></td>
+                                    </tr>
+                                <?php $i = $i + 1;
+                                endforeach; ?>
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th class="text-center"># Registro</th>
+                                    <th class="text-center">Documento</th>
+                                    <th class="text-center">Fecha Registro</th>
+                                    <th class="text-center">Url</th>
+                                    <th class="text-center">Descargar Archivo</th>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        <legend hidden id="nombre"><?= $Nombre_Completo ?></legend>
     </div>
-
+<?php endif; ?>
 </body>
+<script src="js/addons/datatables.min.js"></script>
+<script src="js/tables.js"></script>
 <script>
-    $(document).ready(function () {
+    $(document).ready(function() {
         console.log('JQ')
         document.getElementsByName('descargar').forEach(Element => {
-            Element.addEventListener('click', function (e) {
+            Element.addEventListener('click', function(e) {
                 e.preventDefault()
 
                 console.log(this)
@@ -130,7 +152,7 @@
                     xhrFields: {
                         responseType: 'blob'
                     },
-                    success: function (res) {
+                    success: function(res) {
                         console.log(res)
                         var link = document.createElement('a');
                         link.href = window.URL.createObjectURL(res);
@@ -146,11 +168,9 @@
     })
 </script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
 </script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
 </script>
 
 </html>
