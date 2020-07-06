e929d94eb30e2433808068f89bcc2fbc7afa7b35
Victor Daniel Zabaleta Castellar <zabaletacastellar@gmail.com>
2020-07-06 16:11:27 -0500

    se agrego la validacion para evitar duplicado de registro de pacientes

--- getform1.php
+++ getform1.php
@@ -17,41 +17,49 @@ $exito= '';
 if (!filter_var($correo, FILTER_VALIDATE_EMAIL)) {
   $errores .= 'Esta dirección de correo No es Validad';
 }
-//comprobamos que los campos no esten vacios
-if (empty($tipo_paciente) || empty ($primer_nombre) || empty($primer_apellido) || empty($segundo_apellido)
-|| empty($tipo_documento) || empty($numero_identificacion) || empty($edad) || empty($unidad_medida) || empty($sexo) || empty($barrio) || empty($telefono) || empty($aseguradora)){
-//si hay alguna variable vacia, entonces errores va hacer igual al contenido que tenga la variable
- $errores .= 'Todos los Campos deben estar diligenciados y con el Formato Adecuado';
-}
-else {
-  $consulta =
-  $conexion->prepare(
-  "INSERT INTO pacientes
-  (tipo_paciente, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, tipo_documento, numero_documento, edad, unidad_medida, sexo, barrio, municipio, correo, telefono, aseguradora, fecha_prog_recep, id_usuario)
-  VALUES
-  (:tipo_paciente, :primer_nombre, :segundo_nombre, :primer_apellido, :segundo_apellido, :tipo_documento, :numero_documento, :edad, :unidad_medida, :sexo, :barrio, :municipio, :correo, :telefono, :aseguradora, :fecha_prog_recep, :id_usuario)");
 
-  $consulta->execute(array(
-   ':tipo_paciente'                       => $tipo_paciente,
-   ':primer_nombre'                       => $primer_nombre,
-   ':segundo_nombre'                      => $segundo_nombre,
-   ':primer_apellido'                     => $primer_apellido,
-   ':segundo_apellido'                    => $segundo_apellido,
-   ':tipo_documento'                      => $tipo_documento,
-   ':numero_documento'                    => $numero_identificacion,
-   ':edad'                                => $edad,
-   ':unidad_medida'                       => $unidad_medida,
-   ':sexo'                                => $sexo,
-   ':barrio'                              => $barrio,
-   ':municipio'                           => $municipio,
-   ':correo'                              => $correo,
-   ':telefono'                            => $telefono,
-   ':aseguradora'                         => $aseguradora,
-   ':id_usuario'                          => $id_usuario,
-   ':fecha_prog_recep'                    => $fecha_recepcion_programacion
- ));
- $exito = 'Datos Guardados Exitosamente';
-  //header('Location:index.php');//despues de guardar el usuario en al bd lo redirreccionamos al login para que inicie sessión
+$consulta = $conexion->prepare("SELECT * FROM pacientes WHERE numero_documento = ?");
+$consulta->execute(array($numero_identificacion));
+
+if($consulta->rowCount() == 0){
+  //comprobamos que los campos no esten vacios
+  if (empty($tipo_paciente) || empty ($primer_nombre) || empty($primer_apellido) || empty($segundo_apellido)
+  || empty($tipo_documento) || empty($numero_identificacion) || empty($edad) || empty($unidad_medida) || empty($sexo) || empty($barrio) || empty($telefono) || empty($aseguradora)){
+  //si hay alguna variable vacia, entonces errores va hacer igual al contenido que tenga la variable
+   $errores .= 'Todos los Campos deben estar diligenciados y con el Formato Adecuado';
+  }
+  else {
+    $consulta =
+    $conexion->prepare(
+    "INSERT INTO pacientes
+    (tipo_paciente, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, tipo_documento, numero_documento, edad, unidad_medida, sexo, barrio, municipio, correo, telefono, aseguradora, fecha_prog_recep, id_usuario)
+    VALUES
+    (:tipo_paciente, :primer_nombre, :segundo_nombre, :primer_apellido, :segundo_apellido, :tipo_documento, :numero_documento, :edad, :unidad_medida, :sexo, :barrio, :municipio, :correo, :telefono, :aseguradora, :fecha_prog_recep, :id_usuario)");
+  
+    $consulta->execute(array(
+     ':tipo_paciente'                       => $tipo_paciente,
+     ':primer_nombre'                       => $primer_nombre,
+     ':segundo_nombre'                      => $segundo_nombre,
+     ':primer_apellido'                     => $primer_apellido,
+     ':segundo_apellido'                    => $segundo_apellido,
+     ':tipo_documento'                      => $tipo_documento,
+     ':numero_documento'                    => $numero_identificacion,
+     ':edad'                                => $edad,
+     ':unidad_medida'                       => $unidad_medida,
+     ':sexo'                                => $sexo,
+     ':barrio'                              => $barrio,
+     ':municipio'                           => $municipio,
+     ':correo'                              => $correo,
+     ':telefono'                            => $telefono,
+     ':aseguradora'                         => $aseguradora,
+     ':id_usuario'                          => $id_usuario,
+     ':fecha_prog_recep'                    => $fecha_recepcion_programacion
+   ));
+   $exito = 'Datos Guardados Exitosamente';
+    //header('Location:index.php');//despues de guardar el usuario en al bd lo redirreccionamos al login para que inicie sessión
+  }
+}else{
+  $errores = "Este paciente ya esta registrado";
 }
 
 
