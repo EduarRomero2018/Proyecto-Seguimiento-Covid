292b0acb84202b5dd875384ce499664c8f4bfdb1
Victor Daniel Zabaleta Castellar <zabaletacastellar@gmail.com>
2020-07-09 17:07:26 -0500

    se agrego validacion para casos de registro no encontrados en la tabla

--- validacion_progMuestra.php
+++ validacion_progMuestra.php
@@ -2,34 +2,46 @@
 include 'conexion.php';  // Funciona.
 //APERTURA DE VARIABLES Datos Personales
 
-  foreach ($_REQUEST as $key) {
-    if($key == ''){
-      die(json_encode('empty'));
-    }
+foreach ($_REQUEST as $key) {
+  if ($key == '') {
+    die(json_encode('empty'));
+  }
+}
+
+try {
+  $paciente_id = $_REQUEST['paciente_id'];
+  //comprobamos que los campos no esten vacios
+
+  $stm = $conexion->prepare("SELECT DATE(fecha_programacion) AS fecha_programacion FROM prog_toma_muestra WHERE pacientes_id = ?");
+  $stm->execute(
+    array(
+      $paciente_id
+    )
+  );
+
+  if ($stm->errorInfo()[2] != null) {
+    $err = $stm->errorInfo()[2];
+    die(json_encode(array('err', $err)));
   }
 
-  try {
-    $paciente_id = $_REQUEST['paciente_id'];
-    //comprobamos que los campos no esten vacios
-
-    $stm = $conexion->prepare("SELECT DATE(fecha_programacion) AS fecha_programacion FROM prog_toma_muestra WHERE pacientes_id = ? AND fecha_realizacion IS NULL ");
-    $stm->execute(
-      array(
-        $paciente_id
-    ));
-    
-    if($stm->errorInfo()[2] != null){
-      $err = $stm->errorInfo()[2];
-      die(json_encode(array('err',$err)));
-    }
-
-    if($stm->rowCount() == 1){
-        $res = $stm->fetchAll(PDO::FETCH_OBJ);
-        die(json_encode(array('ok',$res)));
-    }else{
-        die(json_encode(array('bad',null)));
-    }
-
-  }catch (Exception $e) {
-    die($e->getMessage('ERROR: ' + $e));
+  if ($stm->rowCount() == 0) {
+    $res = $stm->fetchAll(PDO::FETCH_OBJ);
+    die(json_encode(array('!found', null)));
   }
+
+  $stm = $conexion->prepare("SELECT DATE(fecha_programacion) AS fecha_programacion FROM prog_toma_muestra WHERE pacientes_id = ? AND fecha_realizacion IS NULL ");
+  $stm->execute(
+    array(
+      $paciente_id
+    )
+  );
+
+  if ($stm->rowCount() == 1) {
+    $res = $stm->fetchAll(PDO::FETCH_OBJ);
+    die(json_encode(array('ok', $res)));
+  } else {
+    die(json_encode(array('bad', null)));
+  }
+} catch (Exception $e) {
+  die($e->getMessage('ERROR: ' + $e));
+}
