51fdf1f359715cb5bc5382180880945e8fbb91bf
Victor Daniel Zabaleta Castellar <zabaletacastellar@gmail.com>
2020-07-09 15:29:27 -0500

    archivo encargado de añadir campos faltantes en programacion de toma de muestra

---
+++ ingreso_progMuestra_complemento.php
@@ -0,0 +1,43 @@
+<?php
+include 'conexion.php';  // Funciona.
+//APERTURA DE VARIABLES Datos Personales
+
+$_REQUEST['observacion'] = empty($_REQUEST['observacion'])? 'sin observacion':$_REQUEST['observacion'];  
+
+foreach ($_REQUEST as $key) {
+    if ($key == '') {
+        die(json_encode('empty'));
+    }
+}
+
+try {
+    $paciente_id = $_REQUEST['paciente_id'];
+    $fecha_realizacion = $_REQUEST['fecha_realizacion'] . ' ' . date('h:i:s');
+    $visita_exitosa = $_REQUEST['visita_exitosa'];
+    $observacion = $_REQUEST['observacion'];
+
+    //comprobamos que los campos no esten vacios
+
+    $stm = $conexion->prepare("UPDATE prog_toma_muestra SET fecha_realizacion = ?, visita_exitosa = ?, observacion = ? WHERE pacientes_id = ?");
+    $stm->execute(
+        array(
+            $fecha_realizacion,
+            $visita_exitosa,
+            $observacion,
+            $paciente_id
+        )
+    );
+
+    if ($stm->errorInfo()[2] != null) {
+        $err = $stm->errorInfo()[2];
+        die(json_encode(array('err', $err)));
+    }
+
+    if ($stm->rowCount() > 0) {
+        die(json_encode(array('ok', null)));
+    } else {
+        die(json_encode(array('bad', null)));
+    }
+} catch (Exception $e) {
+    die($e->getMessage('ERROR: ' + $e));
+}
