43d5e64504ec6fdb754fa5c9a3d4aee6c011573e
Victor Daniel Zabaleta Castellar <zabaletacastellar@gmail.com>
2020-07-09 15:24:19 -0500

    se modifico validacion en caso de no encontrar resultado

--- progmuestra.php
+++ progmuestra.php
@@ -21,21 +21,22 @@ else{
   //VALIDAMOS QUE LA IDENTIFICACION NO EXISTA EN LA BD
   $consulta = $conexion->prepare('SELECT * FROM pacientes WHERE numero_documento = :numero_documento LIMIT 1');
   $consulta->execute(array(':numero_documento' => $documento));
-          $resultado = $consulta->fetch();
-          $tipo_documento=$resultado['tipo_documento'];
-          $numero_documento=$resultado['numero_documento'];
-          $edad=$resultado['edad'];
-          $primer_nombre=$resultado['primer_nombre'];
-          $segundo_nombre=$resultado['primer_apellido'];
-         $nombre_completo = $primer_nombre. ' '. $segundo_nombre;
-          $tipo_paciente=$resultado['tipo_paciente'];
-          $aseguradora=$resultado['aseguradora'];
-          $fecha_registro=$resultado['fecha_registro'];
-          $id=$resultado['id'];
-          //mandamos un msg si no encontro nada
+  $resultado = $consulta->fetch();
           if (empty($resultado)){
-           $errores= 'Paciente no Encontrado'; //enviamos el mensaje de error
+          $errores= 'Paciente no Encontrado'; //enviamos el mensaje de error
+          }else{
+            $tipo_documento=$resultado['tipo_documento'];
+            $numero_documento=$resultado['numero_documento'];
+            $edad=$resultado['edad'];
+            $primer_nombre=$resultado['primer_nombre'];
+            $segundo_nombre=$resultado['primer_apellido'];
+            $nombre_completo = $primer_nombre. ' '. $segundo_nombre;
+            $tipo_paciente=$resultado['tipo_paciente'];
+            $aseguradora=$resultado['aseguradora'];
+            $fecha_registro=$resultado['fecha_registro'];
+            $id=$resultado['id'];
           }
+          //mandamos un msg si no encontro nada
 }
 }
 
