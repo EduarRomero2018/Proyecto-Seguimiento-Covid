b3da2ebf7f7e52c7a25463f3f84fd1e20556f687
Victor Daniel Zabaleta Castellar <zabaletacastellar@gmail.com>
2020-07-23 09:42:11 -0500

    se modifico el archivo para adaptarlo a la funcionalidad del modal

--- validacion_progMuestra.php
+++ validacion_progMuestra.php
@@ -9,10 +9,39 @@ foreach ($_REQUEST as $key) {
 }
 
 try {
-  $paciente_id = $_REQUEST['paciente_id'];
+  $numero_documento = $_REQUEST['identificacion'];
   //comprobamos que los campos no esten vacios
+  $stm = $conexion->prepare(
+    "SELECT 
+    id
+    FROM pacientes
+    WHERE numero_documento = ?"
+  );
+  $stm->execute(
+    array(
+      $numero_documento
+    )
+  );
 
-  $stm = $conexion->prepare("SELECT DATE(fecha_programacion) AS fecha_programacion FROM prog_toma_muestra WHERE pacientes_id = ?");
+  if ($stm->errorInfo()[2] != null) {
+    $err = $stm->errorInfo()[2];
+    die(json_encode(array('err', $err)));
+  }
+  
+  if ($stm->rowCount() == 0) {
+    $res = $stm->fetchAll(PDO::FETCH_OBJ);
+    die(json_encode(array('!found', null)));
+  }
+  
+  $res = $stm->fetch();
+  $paciente_id = $res['id'];
+  
+  $stm = $conexion->prepare(
+    "SELECT 
+    id
+    FROM prog_toma_muestra
+    WHERE pacientes_id = ? AND fecha_realizacion IS NOT NULL"
+  );
   $stm->execute(
     array(
       $paciente_id
@@ -23,13 +52,20 @@ try {
     $err = $stm->errorInfo()[2];
     die(json_encode(array('err', $err)));
   }
-
-  if ($stm->rowCount() == 0) {
+  
+  if ($stm->rowCount() != 0) {
     $res = $stm->fetchAll(PDO::FETCH_OBJ);
-    die(json_encode(array('!found', null)));
+    die(json_encode(array('found', null)));
   }
 
-  $stm = $conexion->prepare("SELECT DATE(fecha_programacion) AS fecha_programacion FROM prog_toma_muestra WHERE pacientes_id = ? AND fecha_realizacion IS NULL ");
+  $stm = $conexion->prepare(
+    "SELECT 
+    DATE(fecha_programacion) AS fecha_programacion,
+    pacientes.id,pacientes.primer_nombre,pacientes.numero_documento
+    FROM prog_toma_muestra
+    RIGHT JOIN pacientes ON  pacientes.id = prog_toma_muestra.pacientes_id
+    WHERE prog_toma_muestra.pacientes_id = ? AND fecha_realizacion IS NULL"
+  );
   $stm->execute(
     array(
       $paciente_id
