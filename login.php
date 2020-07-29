afd8c235d4a5a9859545bb5cd43bfbe00e0871a0
Eduar_Romero <el.g21@hotmail.com>
2020-07-29 08:10:55 -0500

    se agrego ['role'] = ['roles']

--- login.php
+++ login.php
@@ -26,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	if ($resultado !== false) {
 		$_SESSION['usuario'] = $identificacion;
 		$_SESSION['nombre_apellido'] = $resultado['nombre_apellido'];
-		$_SESSION['role'] = $resultado['role'];
+		$_SESSION['role'] = $resultado['roles'];
 		$_SESSION['id'] = $resultado['id'];
 
 		header('Location: index.php');
