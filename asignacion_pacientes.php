e26b2a53fac43edbd9fd3d344afde50997c84889
Victor Daniel Zabaleta Castellar <zabaletacastellar@gmail.com>
2020-07-30 11:39:43 -0500

    se creo archivo de asignacion de pacientes

---
+++ asignacion_pacientes.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once 'conexion.php';
+
+$stm = $conexion->prepare("SELECT id, nombre_apellido, roles FROM usuarios WHERE nombre_apellido != 'sistemas'");
+$stm->execute();
+
+$usuarios = $stm->fetchAll(PDO::FETCH_OBJ);
+
+$stm = $conexion->prepare("SELECT * FROM pacientes WHERE id_usuario IS NULL");
+$stm->execute();
+
+$cantidad_pacientes = $stm->rowCount();
+
+if(isset($_REQUEST['asignar'])){
+    $id_usuario = $_REQUEST['id_usuario'];
+    $cantidad = $_REQUEST['cantidad_pacientes'];
+
+    $stm = $conexion->prepare("SELECT id FROM pacientes WHERE id_usuario IS NULL LIMIT $cantidad");
+    $stm->execute();
+
+    foreach($stm->fetchAll(PDO::FETCH_OBJ) as $paciente){
+        $stm = $conexion->prepare("UPDATE pacientes SET id_usuario = ? WHERE id = $paciente->id");
+        $stm->execute(array($id_usuario));
+
+        if($stm->errorInfo()[2] != NULL){
+            exit(print_r($stm->errorInfo()));
+        }
+    }
+}
+
+require 'views/asignacion_pacientes_view.php';
