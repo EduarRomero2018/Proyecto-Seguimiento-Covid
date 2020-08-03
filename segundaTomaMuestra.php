47a6e5390b4ec96eda595be1ca16f2eefa522432
Victor Daniel Zabaleta Castellar <zabaletacastellar@gmail.com>
2020-08-02 20:28:31 -0500

    se eliminaron los parametros estdo_paciente, fecha_fallecimiento, lugar_fallecimiento

--- segundaTomaMuestra.php
+++ segundaTomaMuestra.php
@@ -7,10 +7,7 @@ try {
     $pacientes_id = $_REQUEST['paciente_id'];
     $usuario_id = $_REQUEST['usuario_id'];
     $visita_domiciliaria = $_REQUEST['visita_domiciliaria'];
-    $estado_paciente = $_REQUEST['estado_paciente'];
-    $fecha_fallecimiento = $_REQUEST['fecha_fallecimiento'];
     $fecha_programacion = $_REQUEST['fecha_programacion'];
-    $lugar_fallecimiento = $_REQUEST['lugar_fallecimiento'];
 
     $stm = $conexion->prepare("SELECT * FROM seguimiento_paciente WHERE id_pacientes = ?");
 
@@ -20,26 +17,13 @@ try {
         die(json_encode(1));
     }
 
-    if($fecha_fallecimiento == ''){
-        $stm = $conexion->prepare("INSERT INTO segunda_toma_muestra_control VALUES(NULL,?,?, NULL, NULL, NULL,?,?,?,NULL,NULL,NOW(),NULL,'ACTIVO','NO',NULL)");
-        $stm->execute(array(
-            $pacientes_id,
-            $fecha_programacion,
-            $visita_domiciliaria,
-            $estado_paciente,
-            $usuario_id
-        ));
-    }else{
-        $stm = $conexion->prepare("INSERT INTO segunda_toma_muestra_control VALUES(NULL,?,NULL, NULL, NULL, NULL,?,?,?,?,?,NOW(),NULL,'ACTIVO','NO',NULL)");
-        $stm->execute(array(
-            $pacientes_id,
-            $visita_domiciliaria,
-            $estado_paciente,
-            $usuario_id,
-            $fecha_fallecimiento,
-            $lugar_fallecimiento
-        ));
-    }
+    $stm = $conexion->prepare("INSERT INTO segunda_toma_muestra_control VALUES(NULL,?,?, NULL, NULL, NULL,?,?,NOW(),NULL,'ACTIVO','NO',NULL)");
+    $stm->execute(array(
+        $pacientes_id,
+        $fecha_programacion,
+        $visita_domiciliaria,
+        $usuario_id
+    ));
 
     if ($stm->errorInfo()[2] != null) {
         $err = $stm->errorInfo()[2];
