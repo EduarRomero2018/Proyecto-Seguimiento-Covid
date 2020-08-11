169a1d8425ca54facb1dbc1182f8cfa0c193eb46
Victor Daniel Zabaleta Castellar <zabaletacastellar@gmail.com>
2020-08-11 15:53:23 -0500

    cambio de color en texto

--- views/asignacion2_view.php
+++ views/asignacion2_view.php
@@ -42,7 +42,7 @@
                         <div class="form-group" name="hidden" hidden>
                             <label>Cantidad de pacientes que desea asignar</label>
                             <input type="number" id="cantidad_pacientes" class="form-control" min="1">
-                            <h3 id="cantidad_pacientes_text"></h3>
+                            <h5 id="cantidad_pacientes_text" style="color: rgba(6, 62, 131, 0.9);" class="mt-3"></h5>
                         </div>
                         <div class="row">
                             <div class="form-group col-sm-7 pl-2 pr-0" name="hidden" hidden>
