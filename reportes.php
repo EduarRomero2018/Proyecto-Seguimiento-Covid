3cc3296c9708a7f6cfb60ccb68e700f04306b1d5
Eduar_Romero <el.g21@hotmail.com>
2020-08-12 07:29:48 -0500

    se agregaron las consultas de las nuevas card del dasboard

--- reportes.php
+++ reportes.php
@@ -1,7 +1,7 @@
 <?php
 include 'conexion.php';
 $id = $_SESSION['id'];
-/* PACIENTES PENDIENTES POR RESULTADOS*/
+// pacientes pendientes por resultados
 $consulta = $conexion->prepare("SELECT COUNT(*) AS Numero_Pacientes
 FROM prog_toma_muestra
 WHERE resultado = 'Pendiente'");
@@ -10,7 +10,7 @@ $res = $consulta ->fetch();
 $numero_conteo = $res['Numero_Pacientes'];
    //var_dump ($consulta->errorInfo())  ;
 
-  /* PACIENTES QUE NO SE LE HAN REALIZADO TOMA DE MUESTRA*/
+  // pacientes que no se le han realizado toma de muestra
 $consulta = $conexion->prepare("SELECT COUNT(*) AS Cantidad_Pacientes
 FROM pacientes
 WHERE id NOT IN
@@ -20,7 +20,7 @@ $res = $consulta ->fetch();
 $Cantidad_Pacientes = $res['Cantidad_Pacientes'];
 //print_r ($consulta->errorInfo())  ;
 
-/* CANTIDAD DE PACIENTES POSITIVOS*/
+// cantidad de pacientes positivos
 $consulta = $conexion->prepare("SELECT * FROM prog_toma_muestra WHERE resultado = 'Positivo'");
 $consulta->execute();
 
@@ -40,7 +40,26 @@ $consulta->execute();
 $res = $consulta ->fetch();
 $Cantidad_Pacientes_programados = $res['Cantidad_Pacientes'];
 
-  /* CANTIDAD DE PACIENTES PROGRAMADOS PERO QUE ESTEN PENDIENTES POR REALIZARLE LA PRUEBA*/
+/* Cantidad de kits entregados*/
+$consulta = $conexion->prepare("SELECT COUNT(*) AS Cantidad_kits
+FROM seguimiento_paciente
+WHERE entrega_kits = 'Si'");
+$consulta->execute();
+$res = $consulta ->fetch();
+$cantidad_kits = $res['Cantidad_kits'];
+// var_dump($cantidad_kits);
+
+/* Cantidad de pacientes fallecidos*/
+$consulta = $conexion->prepare("SELECT COUNT(*) AS pacientes_fallecidos
+FROM pacientes
+WHERE estado_paciente = 'MUERTO'");
+$consulta->execute();
+$res = $consulta ->fetch();
+$pacientes_fallecidos = $res['pacientes_fallecidos'];
+// print_r($cantidad_kits);
+
+
+  // cantidad de pacientes programados pero que estan pendientes por resultados
   $consulta = $conexion->prepare("SELECT COUNT(*) AS Cantidad_p_p_pendiente_por_toma
   FROM prog_toma_muestra
   WHERE fecha_programacion IS NOT NULL
@@ -50,24 +69,25 @@ $Cantidad_Pacientes_programados = $res['Cantidad_Pacientes'];
   $Cantidad_p_p_pendiente_por_toma= $res['Cantidad_p_p_pendiente_por_toma'];
   //print_r ($consulta->errorInfo())  ;
 
+  /* Cantidad de pacientes Asintomaticos*/
   $consulta = $conexion->prepare(
-    "SELECT id_pacientes AS numero_de_asintomaticos  
-    FROM seguimiento_paciente  
-    WHERE asintomatico = 'Si' AND actual = 'si' 
+    "SELECT id_pacientes AS numero_de_asintomaticos
+    FROM seguimiento_paciente
+    WHERE asintomatico = 'Si' AND actual = 'si'
     GROUP BY id_pacientes"
   );
   $consulta->execute();
-
   $asintomaticos = $consulta->rowCount();
+  // print_r($asintomaticos);
 
+  /* Cantidad de pacientes Sintomaticos*/
   $consulta = $conexion->prepare(
-    "SELECT id_pacientes AS numero_de_asintomaticos  
-    FROM seguimiento_paciente  
-    WHERE asintomatico = 'No' AND actual = 'si' 
+    "SELECT id_pacientes AS numero_de_asintomaticos
+    FROM seguimiento_paciente
+    WHERE asintomatico = 'No' AND actual = 'si'
     GROUP BY id_pacientes"
   );
   $consulta->execute();
-
   $sintomaticos = $consulta->rowCount();
 
 /* --CONSULTA PARA SABER CUALES SON LOS PACIENTES QUE SE LE HAN REALIZADO TOMA DE MUESTRA--
@@ -87,5 +107,3 @@ LEFT JOIN complemento_seg ON complemento_seg_id = complemento_seg.id_pacientes
 LEFT JOIN pacientes ON complemento_seg_id = complemento_seg.id
 WHERE complemento_seg_id = 6
 */
-
-?>
