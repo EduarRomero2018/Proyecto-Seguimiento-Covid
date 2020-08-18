7bc836fb7a4aa55fd20e2e3b7a5973eaa76a392d
Eduar_Romero <el.g21@hotmail.com>
2020-08-18 16:14:03 -0500

    se agrego cpn.php y su vista para mostrar pacientes negativos

---
+++ cpn.php
@@ -0,0 +1,14 @@
+<?php session_start(); //vamos a trabajar con sessiones
+include 'conexion.php';
+
+    $consulta = $conexion->prepare(
+        "SELECT CONCAT(primer_nombre, ' ', primer_apellido) AS 'Nombre_Completo', tipo_documento,edad,
+        numero_documento,DATE(fecha_programacion) AS fecha_programacion, PTM.fecha_entrega_lab ,fecha_resultado
+        FROM pacientes P
+        RIGHT JOIN prog_toma_muestra PTM ON P.id = PTM.pacientes_id
+        WHERE resultado = 'Negativo' AND estado_paciente = 'VIVO'");
+
+    $consulta->execute();
+    $res = $consulta->fetchAll(PDO::FETCH_OBJ);
+
+    require 'views/cpn_view.php';
