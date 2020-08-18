dd5fb7a8912a59ed2be6dbf3951f415ea0cc77a1
Victor Daniel Zabaleta Castellar <zabaletacastellar@gmail.com>
2020-08-18 15:57:08 -0500

    correccion de codigo

--- cambioEstado.php
+++ cambioEstado.php
@@ -7,8 +7,12 @@ if(empty($_REQUEST['fecha_f'])){
 }
 
 $id = $_REQUEST['id'];
+$fecha_f = $_REQUEST['fecha_f'] . ' ' . date('h:i:s');
 
-$stm = $conexion->prepare("UPDATE pacientes SET estado_paciente = 'MUERTO', fecha_fallecimiento = NOW()  WHERE id = ?");
-$stm->execute(array($id));
+$stm = $conexion->prepare("UPDATE pacientes SET estado_paciente = 'MUERTO', fecha_fallecimiento = ?  WHERE id = ?");
+$stm->execute(array(
+    $fecha_f,
+    $id
+));
 die(json_encode('ok'));
 ?>
