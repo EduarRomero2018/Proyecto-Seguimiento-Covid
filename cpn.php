39ee21ab601cc4da965c535c59af8d104f06104b
Victor Daniel Zabaleta Castellar <zabaletacastellar@gmail.com>
2020-08-19 08:44:01 -0500

    se extrajo el valor de notificado del paciente

--- cpn.php
+++ cpn.php
@@ -3,7 +3,7 @@ include 'conexion.php';
 
     $consulta = $conexion->prepare(
         "SELECT p.id, CONCAT(primer_nombre, ' ', primer_apellido) AS 'Nombre_Completo', tipo_documento,edad,telefono,telefono2,
-        numero_documento,DATE(fecha_programacion) AS fecha_programacion, PTM.fecha_entrega_lab ,fecha_resultado
+        numero_documento,DATE(fecha_programacion) AS fecha_programacion, PTM.fecha_entrega_lab ,fecha_resultado, PTM.notificado
         FROM pacientes P
         RIGHT JOIN prog_toma_muestra PTM ON P.id = PTM.pacientes_id
         WHERE resultado = 'Negativo' AND estado_paciente = 'VIVO'");
