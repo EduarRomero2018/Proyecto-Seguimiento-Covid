476113eb00c2df21bb751bf58352744a56837dd4
Victor Daniel Zabaleta Castellar <zabaletacastellar@gmail.com>
2020-08-19 08:49:17 -0500

    se creo campo para visualizar el estado de notificacion de pacientes

--- views/cantidad_p_p_view.php
+++ views/cantidad_p_p_view.php
@@ -41,6 +41,7 @@
                                     <th style="background: #ffc974" class="text-center th-sm">Fecha de Programacion<i</th>
                                     <th style="background: #ffc974" class="text-center th-sm">Fecha de entrega laboratorio<i</th>
                                     <th style="background: #ffc974" class="text-center th-sm">Fecha Del Resultado</th>
+                                    <th style="background: #ffc974" class="text-center th-sm">Paciente Notificado</th>
                                 </tr>
                             </thead>
                             <tbody id="tbl-llamadas">
@@ -56,6 +57,7 @@
                                         <td class="text-center"><?= $key->fecha_programacion ?></td>
                                         <td class="text-center"><?= $key->fecha_entrega_lab ?></td>
                                         <td class="text-center"><?= $key->fecha_resultado ?></td>
+                                        <td class="text-center"><?= $key->notificado ?></td>
                                     </tr>
                                 <?php $i++;
                                 endforeach; ?>
@@ -70,6 +72,7 @@
                                     <th style="background: #ffc974" class="text-center th-sm">Fecha de Programacion<i</th>
                                     <th style="background: #ffc974" class="text-center th-sm">Fecha de entrega laboratorio<i</th>
                                     <th style="background: #ffc974" class="text-center th-sm">Fecha Del Resultado</th>
+                                    <th class="text-center">Paciente Notificado</th>
                                 </tr>
                                 </tr>
                             </tfoot> -->
