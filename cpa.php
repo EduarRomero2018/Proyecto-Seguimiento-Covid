4019724ab6555ced1fd5da7b355cde4b82a9d16b
Eduar_Romero <el.g21@hotmail.com>
2020-08-21 08:47:40 -0500

    se descomento el codigo de los switch

--- cpa.php
+++ cpa.php
@@ -1,27 +1,27 @@
 <?php session_start(); //vamos a trabajar con sessiones
 include 'conexion.php';
-// switch ($_SESSION['role']) {
-//     case 'Coordinador covid':
-//         $filtro = "";
-//         break;
+switch ($_SESSION['role']) {
+    case 'Coordinador covid':
+        $filtro = "";
+        break;
 
-//     case 'Auxiliar de programacion':
-//         $id_session = $_SESSION['id'];
-//         $filtro = "AND id_usuario = $id_session";
-//         break;
-//     case 'Auxiliar de seguimiento':
-//         $id_session = $_SESSION['id'];
-//         $filtro = "AND id_usuario_seguimiento = $id_session";
-//         break;
-//     case 'Digitador':
-//         $id_session = $_SESSION['id'];
-//         $filtro = "AND id_usuario_resultado = $id_session";
-//         break;
-//     case 'Medico':
-//         $id_session = $_SESSION['id'];
-//         $filtro = "AND id_usuario_notificacion = $id_session AND prog_toma_muestra.resultado = 1";
-//         break;
-// }
+    case 'Auxiliar de programacion':
+        $id_session = $_SESSION['id'];
+        $filtro = "AND id_usuario = $id_session";
+        break;
+    case 'Auxiliar de seguimiento':
+        $id_session = $_SESSION['id'];
+        // $filtro = "AND id_usuario_seguimiento = $id_session";
+        break;
+    case 'Digitador':
+        $id_session = $_SESSION['id'];
+        $filtro = "AND id_usuario_resultado = $id_session";
+        break;
+    case 'Medico':
+        $id_session = $_SESSION['id'];
+        $filtro = "AND id_usuario_notificacion = $id_session AND prog_toma_muestra.resultado = 1";
+        break;
+}
             $consulta = $conexion->prepare(
                 "SELECT CONCAT(primer_nombre, ' ', primer_apellido) AS 'Nombre_Completo', P.tipo_documento, P.edad, P.numero_documento
                 FROM seguimiento_paciente SP
@@ -33,3 +33,4 @@ include 'conexion.php';
             $res = $consulta->fetchAll(PDO::FETCH_OBJ);
 
             require 'views/cpa_view.php';
+            ?>
