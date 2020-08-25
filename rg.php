eaa36636371606342ecada0aab87542709408a28
Eduar_Romero <el.g21@hotmail.com>
2020-08-25 10:43:41 -0500

    se esta probando switch para el tipo de reporte

--- rg.php
+++ rg.php
@@ -7,14 +7,36 @@ $exito= '';
 $tipo_de_reporte = $_POST['tipo_reporte'];
 $fecha_inicio_reporte = $_POST['fecha_inicio_reporte'];
 $fecha_final_reporte = $_POST['fecha_final_reporte'];
+
 if (empty($tipo_de_reporte)){
     $errores .= 'Debe Seleccionar por lo menos un Reporte';
   }
 if (empty($fecha_inicio_reporte) || empty($fecha_final_reporte) || empty($fecha_final_reporte)){
-    $errores .= 'Las fechas son Obligatorias';
+    $errores = 'Las fechas son Obligatorias';
   }
-}
-else {
+//   else {
+    switch ($tipo_de_reporte) {
+
+      case 'CPP':
+        $consulta = $conexion->prepare(
+          "SELECT CONCAT(primer_nombre, ' ', primer_apellido) AS 'Nombre_Completo', tipo_documento,edad,
+          numero_documento,
+          CONCAT(telefono, '-', telefono2) AS 'telefonos'
+          FROM pacientes P
+          RIGHT JOIN prog_toma_muestra PTM ON P.id = PTM.pacientes_id
+          WHERE resultado = 'Positivo' AND estado_paciente = 'VIVO'");
+        $consulta->execute();
+        $res = $consulta->fetchAll(PDO::FETCH_OBJ);
+        // print_r($res);
+        break;
+
+      default:
+        # code...
+        break;
+    }
+//   }
+// }
+// else {
 
 
 }
