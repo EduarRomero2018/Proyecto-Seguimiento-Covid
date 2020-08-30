4dfe36c7054b47584e6cc0b3b9a10bc01b0facf7
Eduar_Romero <el.g21@hotmail.com>
2020-08-29 20:50:00 -0500

    se concateno los telefonos y se agregaron mas casos de prueba

--- rg.php
+++ rg.php
@@ -21,7 +21,7 @@ if (empty($fecha_inicio_reporte) || empty($fecha_final_reporte) || empty($fecha_
         $consulta = $conexion->prepare(
           "SELECT CONCAT(primer_nombre, ' ', primer_apellido) AS 'Nombre_Completo', tipo_documento,edad,
           numero_documento,
-          CONCAT(telefono, '-', telefono2) AS 'telefonos'
+          CONCAT(telefono, '-', telefono2) AS 'telefonos', barrio
           FROM pacientes P
           RIGHT JOIN prog_toma_muestra PTM ON P.id = PTM.pacientes_id
           WHERE resultado = 'Positivo' AND estado_paciente = 'VIVO'");
@@ -29,6 +29,47 @@ if (empty($fecha_inicio_reporte) || empty($fecha_final_reporte) || empty($fecha_
         $res = $consulta->fetchAll(PDO::FETCH_OBJ);
         // print_r($res);
         break;
+
+        case 'CPN':
+          $consulta = $conexion->prepare(
+            "SELECT roles FROM usuarios
+            WHERE identificacion = '1047433073'");
+          $consulta->execute();
+          $res = $consulta->fetchAll(PDO::FETCH_OBJ);
+          // print_r($res);
+          break;
+
+          case 'CPS':
+            $consulta = $conexion->prepare(
+              "SELECT COUNT(*) FROM usuarios");
+            $consulta->execute();
+            $res = $consulta->fetchAll(PDO::FETCH_OBJ);
+            // print_r($res);
+            break;
+
+            case 'CPA':
+              $consulta = $conexion->prepare(
+                "SELECT COUNT(*) FROM pacientes");
+              $consulta->execute();
+              $res = $consulta->fetchAll(PDO::FETCH_OBJ);
+              // print_r($res);
+              break;
+
+            case 'CPF':
+              $consulta = $conexion->prepare(
+                "SELECT COUNT(*) FROM prog_toma_muestra");
+              $consulta->execute();
+              $res = $consulta->fetchAll(PDO::FETCH_OBJ);
+              // print_r($res);
+              break;
+
+            case 'CMP':
+              $consulta = $conexion->prepare(
+                "SELECT COUNT(*) FROM seguimiento_paciente");
+              $consulta->execute();
+              $res = $consulta->fetchAll(PDO::FETCH_OBJ);
+              // print_r($res);
+              break;
 
       default:
         # code...
