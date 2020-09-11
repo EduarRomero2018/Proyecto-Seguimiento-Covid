947c48d5ec9bacf5f49c144bff4ce9b39a4e4ef5
Victor Daniel Zabaleta Castellar <zabaletacastellar@gmail.com>
2020-09-11 10:01:33 -0500

    archivo encargado de actualizar los datos basicos del paciente

---
+++ actualizacionPaciente.php
@@ -0,0 +1,95 @@
+<?php
+session_start();
+require_once 'conexion.php';
+
+if (isset($_REQUEST['buscar']) && $_REQUEST['buscar']) 
+{
+    $cedula = $_REQUEST['Bcedula'];
+
+    $stm = $conexion->prepare("SELECT *,DATE(fecha_fallecimiento) AS fecha_fallecimiento FROM pacientes WHERE numero_documento = ?");
+    $stm->execute(array($cedula));
+
+    $DatosPaciente = $stm->fetchAll(PDO::FETCH_OBJ);
+    foreach ($DatosPaciente as $paciente ) {}
+}
+
+if(isset($_REQUEST['actualizar']) && $_REQUEST['actualizar'])
+{
+    $id = $_REQUEST['id'];
+    $tipo_documento = $_REQUEST['tipo_documento'];
+    $numero_documento = $_REQUEST['cedula'];
+    $primer_nombre = $_REQUEST['primer_nombre']; 
+    $segundo_nombre = $_REQUEST['segundo_nombre']; 
+    $primer_apellido = $_REQUEST['primer_apellido']; 
+    $segundo_apellido = $_REQUEST['segundo_apellido'];
+    $tipo_paciente = $_REQUEST['tipo_paciente'];
+    $edad = $_REQUEST['edad'];
+    $unidad_medida = $_REQUEST['unidad_medida'];
+    $sexo = $_REQUEST['sexo'];
+    $barrio = $_REQUEST['barrio'];
+    $municipio = $_REQUEST['municipio'];
+    $correo = $_REQUEST['correo']; 
+    $telefono = $_REQUEST['telefono']; 
+    $telefono2 = $_REQUEST['telefono2']; 
+    $aseguradora = $_REQUEST['aseguradora']; 
+    $regimen = $_REQUEST['regimen']; 
+    $estado_paciente = $_REQUEST['estado_paciente']; 
+    $fecha_fallecimiento = empty($_REQUEST['fecha_fallecimiento']) ? NULL : $_REQUEST['fecha_fallecimiento'];
+
+    $stm = $conexion->prepare(
+        "UPDATE pacientes SET 
+        primer_nombre = ?, 
+        tipo_paciente = ?, 
+        segundo_nombre = ?, 
+        primer_apellido = ?, 
+        segundo_apellido = ?, 
+        tipo_documento = ?, 
+        numero_documento = ?, 
+        edad = ?, 
+        unidad_medida = ?, 
+        sexo = ?,
+        barrio = ?, 
+        municipio = ?, 
+        correo = ?, 
+        telefono = ?, 
+        telefono2 = ?, 
+        aseguradora = ?, 
+        regimen = ?, 
+        estado_paciente = ?,
+        fecha_fallecimiento = ?
+        WHERE id = ?");
+    $stm->execute(array(
+        $primer_nombre,
+        $tipo_paciente,
+        $segundo_nombre,
+        $primer_apellido,
+        $segundo_apellido,
+        $tipo_documento,
+        $numero_documento,
+        $edad,
+        $unidad_medida,
+        $sexo,
+        $barrio,
+        $municipio,
+        $correo,
+        $telefono,
+        $telefono2,
+        $aseguradora,
+        $regimen,
+        $estado_paciente,
+        $fecha_fallecimiento,
+        $id
+    ));
+
+    if($stm->errorInfo()[2] != null)
+    {
+        $error = $stm->errorInfo()[2];
+        echo $error;
+    }
+
+    if($stm->rowCount() > 0){
+        $success = 'ok';
+    }
+}
+
+require_once 'views/actualizacionPacientes_view.php';
