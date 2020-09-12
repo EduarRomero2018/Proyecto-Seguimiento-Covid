50a345f37e1e71417a29f2c8b160d32d91d1dc4a
Victor Daniel Zabaleta Castellar <zabaletacastellar@gmail.com>
2020-09-12 09:17:49 -0500

    se añadio un caso para seguimiento y medico

--- lista_pacientes_mutual.php
+++ lista_pacientes_mutual.php
@@ -4,18 +4,38 @@ require_once 'conexion.php';
 
 $fecha_realizacion = $_REQUEST['fecha_realizacion'];
 $municipio = $_REQUEST['municipio'];
+$proceso = $_REQUEST['proceso'];
 
-$todos_pacientes = "SELECT CONCAT(p.primer_nombre, ' ', p.primer_apellido) AS 'Nombre_Completo',
-p.tipo_documento, p.numero_documento,
-fecha_programacion, DATE(fecha_realizacion) as fecha_realizacion , p.aseguradora, p.municipio
-FROM prog_toma_muestra ptm
-LEFT JOIN pacientes p ON ptm.pacientes_id = p.id
-WHERE DATE(fecha_realizacion) = ?
-AND fecha_programacion IS NOT NULL
-AND id_usuario_seguimiento IS NULL
-AND estado_paciente = 1
-AND p.aseguradora = 'MUTUAL SER'
-AND municipio = ?";
+switch ($proceso) {
+    case 'seguimiento':
+        $todos_pacientes = "SELECT CONCAT(p.primer_nombre, ' ', p.primer_apellido) AS 'Nombre_Completo',
+        p.tipo_documento, p.numero_documento,
+        fecha_programacion, DATE(fecha_realizacion) as fecha_realizacion , p.aseguradora, p.municipio
+        FROM prog_toma_muestra ptm
+        LEFT JOIN pacientes p ON ptm.pacientes_id = p.id
+        WHERE DATE(fecha_realizacion) = ?
+        AND fecha_programacion IS NOT NULL
+        AND id_usuario_seguimiento IS NULL
+        AND estado_paciente = 1
+        AND p.aseguradora = 'MUTUAL SER'
+        AND municipio = ?";
+        break;
+    
+    case 'medico':
+        $todos_pacientes = "SELECT CONCAT(p.primer_nombre, ' ', p.primer_apellido) AS 'Nombre_Completo',
+        p.tipo_documento, p.numero_documento,
+        fecha_programacion, DATE(fecha_realizacion) as fecha_realizacion , p.aseguradora, p.municipio
+        FROM prog_toma_muestra ptm
+        LEFT JOIN pacientes p ON ptm.pacientes_id = p.id
+        WHERE DATE(fecha_realizacion) = ?
+        AND fecha_programacion IS NOT NULL
+        AND id_usuario_notificacion IS NULL
+        AND estado_paciente = 1
+        AND ptm.resultado = 1
+        AND p.aseguradora = 'MUTUAL SER'
+        AND municipio = ?";
+        break;
+}
 
 
 $stm = $conexion->prepare($todos_pacientes);
