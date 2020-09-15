8450e472d2fa87ca3796090780d2d07cb8172fe4
Eduar_Romero <el.g21@hotmail.com>
2020-09-15 16:23:03 -0500

    codigo actualizado con el de produccion

--- listar_pacientes.php
+++ listar_pacientes.php
@@ -25,32 +25,35 @@ if (!isset($_REQUEST['consulta'])) {
             break;
         case 'Digitador':
             $id_session = $_SESSION['id'];
-            $filtro = "AND prog_toma_muestra.resultado = 3";
+            $filtro = "";
             break;
         case 'Medico':
             $id_session = $_SESSION['id'];
             $filtro = "AND id_usuario_notificacion = $id_session AND prog_toma_muestra.resultado = 1";
             break;
     }
-    $sede = strtoupper($_SESSION['sede']);
+
     $usuario_id = $_SESSION['id'];
     $consulta = "SELECT pacientes.id, CONCAT(primer_nombre, ' ', primer_apellido) AS 'Nombre_Completo',
-    CONCAT(edad, ' ', unidad_medida) AS 'Edad', aseguradora,
-    CONCAT(tipo_documento, ' - ', numero_documento) AS 'Identificacion', telefono, barrio,
-    DATE(fecha_programacion) AS fecha_programacion,
-    DATE(fecha_realizacion) AS fecha_realizacion, motivo, fecha_resultado, municipio, resultado, programacion_atencion,
-	UP.nombre_apellido AS usuario_programacion,
-	US.nombre_apellido AS usuario_seguimiento,
-	UR.nombre_apellido AS usuario_resultado,
-	UM.nombre_apellido AS usuario_medico
-	FROM pacientes
-    LEFT JOIN usuarios U ON pacientes.id_usuario = U.id
-    LEFT JOIN usuarios UP ON pacientes.id_usuario = UP.id
-    LEFT JOIN usuarios US ON pacientes.id_usuario_seguimiento = US.id
-    LEFT JOIN usuarios UR ON pacientes.id_usuario_resultado = UR.id
-    LEFT JOIN usuarios UM ON pacientes.id_usuario_notificacion = UM.id
-    LEFT JOIN prog_toma_muestra ON pacientes.id = prog_toma_muestra.pacientes_id
-    WHERE pacientes.estado_paciente = 'VIVO' AND aseguradora = 'MUTUAL SER' AND municipio LIKE '$sede%' AND prog_toma_muestra.resultado = 3 AND prog_toma_muestra.resultado != 2 $filtro";
+CONCAT(edad, ' ', unidad_medida) AS 'Edad', aseguradora,
+CONCAT(tipo_documento, ' - ', numero_documento) AS 'Identificacion', telefono, barrio,
+DATE(fecha_programacion) AS fecha_programacion,
+DATE(fecha_realizacion) AS fecha_realizacion, motivo, fecha_resultado, municipio, resultado, programacion_atencion,
+UP.nombre_apellido AS usuario_programacion,
+US.nombre_apellido AS usuario_seguimiento,
+UR.nombre_apellido AS usuario_resultado,
+UM.nombre_apellido AS usuario_medico
+FROM pacientes
+LEFT JOIN usuarios U ON pacientes.id_usuario = U.id
+LEFT JOIN usuarios UP ON pacientes.id_usuario = UP.id
+LEFT JOIN usuarios US ON pacientes.id_usuario_seguimiento = US.id
+LEFT JOIN usuarios UR ON pacientes.id_usuario_resultado = UR.id
+LEFT JOIN usuarios UM ON pacientes.id_usuario_notificacion = UM.id
+LEFT JOIN prog_toma_muestra ptm ON pacientes.id = ptm.pacientes_id
+WHERE pacientes.estado_paciente = 1
+AND ptm.resultado != 2
+AND aseguradora = 'MUTUAL SER' $filtro";
+
     $query = $conexion->prepare($consulta);
 
     $query->execute();
