e00e6fbe74aa9ace96964eeed79b9989165af901
Victor Daniel Zabaleta Castellar <zabaletacastellar@gmail.com>
2020-09-15 09:30:06 -0500

    se cambio el formulario de la vista por el formulario de progmuestra_view.php

--- views/toma_muestra_control_view.php
+++ views/toma_muestra_control_view.php
@@ -91,53 +91,66 @@
                 <div class="row">
                     <div class="col-sm-3">
                         <form id="form-TM">
-                            <label class="col-form-label">Visita Domiciliaria</label>
-                            <select id="visita_domiciliaria" class="custom-select">
+                            <label class="col-form-label">Acepta Visita ?</label>
+                            <select id="acepta_visita" class="custom-select">
                                 <option selected value=""> </option>
                                 <option value="SI">Si</option>
                                 <option value="NO">No</option>
-                                <option value="LLAMADA NO Exitosa">llamada no Exitosa</option>
-                                <option value="NO APLICA">No Aplica</option>
+                                <option value="LLAMADA NO EXITOSA">Llamada no Exitosa</option>
+                                <option value="NO APLICA PARA CERCO">No aplica para (Cerco)</option>
+                                <option value="SEDE DE CAMINOS">Sede Caminos IPS</option>
                             </select>
                     </div>
+                    <div class="col-sm-3">
+                        <label class="col-form-label">Fecha de Programacion</label>
+                        <input type="date"  min="<?= date('Y-m-d') ?>" id="fecha_programacion" class="form-control">
+                    </div>
+
 
                     <div class="col-sm-3">
-                        <label class="col-form-label">Estado Del Paciente</label>
-                        <select id="estado_paciente" class="custom-select">
+                        <label class="col-form-label">lugar de Toma de Muestra</label>
+                        <select id="programacion_atencion" class="custom-select">
                             <option selected value=""> </option>
-                            <option value="VIVO">Vivo</option>
-                            <!-- <option value="MUERTO">Muerto</option> -->
+                            <option value="CERCO">Cerco</option>
+                            <option value="MAS 60">Mas 60</option>
+                            <option value="HOSPITALARIO">Hospitalario</option>
+                            <option value="DOMICILIO">Domicilio</option>
+                            <option value="CARCEL">Carcel</option>
+                            <option value="SEDE DE CAMINOS">Caminos IPS</option>
+                            <option value="INTRAMURO 1">Intramuro 1</option>
+                            <option value="INTRAMURO 2">Intramuro 2</option>
                         </select>
                     </div>
 
-                    <div class="col-sm-3" id="programacion" hidden>
-                        <label class="col-form-label">Fecha de Programacion</label>
-                        <input type="date" id="fecha_programacion" min="<?= date('Y-m-d') ?>" class="form-control">
+                    <div class="col-sm-3">
+                        <!-- <br> -->
+                        <label class="col-form-label">Programa al que Pertenece</label>
+                        <select id="nombre_programa" class="custom-select">
+                            <option selected value=""> </option>
+                            <option value="NO APLICA">No Aplica</option>
+                            <option value="NINGUNO">Ninguno</option>
+                            <option value="DE TODO CORAZON">De todo Corazón</option>
+                            <option value="VIH">Vih</option>
+                            <option value="AMARTE (ARTRITIS REUMATOIDES)">Amarte (artritis reumatoides)</option>
+                            <option value="RESPIRA (EPOC)">Respira (Epoc)</option>
+                            <option value="PROMOCION Y MANTENIMIENTO DE LA SALUD">Promocion y mantenimiento de la salud</option>
+                            <option value="MUJER SANA">Mujer Sana</option>
+                            <option value="OBESIDAD">Obesidad</option>
+                            <option value="GESTANTES">Gestantes</option>
+                        </select>
                     </div>
-
-                    <!-- <div class="col-sm-3" name="fallecimiento" hidden>
-                        <label class="col-form-label">Fecha de Fallecimiento</label>
-                        <input type="date" id="fecha_fallecimiento" class="form-control">
+                    <div class="col-sm-3">
+                        <br>
+                        <button id="guardar-p" type="submit" class="btn btn-outline-secondary btn-lg">Guardar Datos</button>
                     </div>
-                    <div class="col-sm-3" name="fallecimiento" hidden>
-                        <label class="col-form-label">Lugar de Fallecimiento</label>
-                        <input type="text" id="lugar_fallecimiento" class="form-control">
-                    </div> -->
                     <div class="col-sm-3">
                         <br>
                         <input type="hidden" id="paciente_id" value="<?= $id ?>">
-                        <input type="hidden" id="usuario_id" value="<?= $_SESSION['id'] ?>">
-                        <!-- <label class="col-form-label">Observacion</label>
-                            <input type="text" id="observacion" class="form-control" placeholder="Observacion"> -->
+                        <input type="hidden" id="programacion_2" value="2">
                     </div>
-                </div>
+                    </form>
 
-                <div class="col-sm-3">
-                    <br>
-                    <button id="guardar" type="submit" class="btn btn-outline-secondary btn-lg">Guardar
-                        Datos</button>
                 </div>
-                </form>
 
             </div>
         </div>
@@ -174,144 +187,4 @@
         </script>
     <?php endif; ?>
 
-    </footer>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js">
-    </script>
-    <!-- Bootstrap core JavaScript -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    </script>
-    <script>
-        $('#guardar').on('click', function(e) {
-            e.preventDefault()
-
-            let visita_domiciliaria = $('#visita_domiciliaria').val()
-            let estado_paciente = $('#estado_paciente').val()
-            let fecha_programacion = $('#fecha_programacion').val()
-            let fecha_fallecimiento = $('#fecha_fallecimiento').val()
-            let lugar_fallecimiento = $('#lugar_fallecimiento').val()
-            let paciente_id = $('#paciente_id').val()
-            let usuario_id = $('#usuario_id').val()
-
-            if (estado_paciente == 'VIVO') {
-                fecha_fallecimiento = ''
-                lugar_fallecimiento = 'Null'
-            } else {
-                fecha_programacion = ''
-            }
-
-            let datos = {
-                visita_domiciliaria,
-                estado_paciente,
-                fecha_programacion,
-                fecha_fallecimiento,
-                lugar_fallecimiento,
-                paciente_id,
-                usuario_id
-            }
-
-            let i = 1
-            for (const key in datos) {
-                if (datos.hasOwnProperty(key)) {
-                    const element = datos[key];
-                    
-                    console.log(element);
-                    if (element == '') {
-                        i ++
-                    }
-                }
-            }
-
-            if (i > 2) {
-                swal({
-                    type: 'error',
-                    title: "Error",
-                    text: "Todos los campos son obligatorios",
-                    button: "Aceptar",
-                    icon: "error",
-                    button: "Aceptar",
-                    animation: false,
-                    customClass: 'animated heartBeat'
-                })
-            }else{
-
-                $.ajax({
-                    type: 'post',
-                    url: 'segundaTomaMuestra.php',
-                    data: datos,
-                    success: function(res) {
-                        console.log(res)
-                        let resultado = JSON.parse(res)
-                        console.log(resultado)
-                        switch (resultado) {
-                            case 1:
-                                swal({
-                                    type: 'error',
-                                    title: "Error",
-                                    text: "Este paciente no cuenta con seguimiento por favor verfique",
-                                    button: "Aceptar",
-                                    icon: "error",
-                                    button: "Aceptar",
-                                    timer: 7000,
-                                    animation: false,
-                                    customClass: 'animated heartBeat'
-                                })
-                                break;
-                            case 'ok':
-                                swal({
-                                    type: 'success',
-                                    title: "Exito",
-                                    text: "Datos guardados correctamente",
-                                    button: "Aceptar",
-                                    icon: "success",
-                                    button: "Aceptar",
-                                    timer: 7000,
-                                    animation: false,
-                                    customClass: 'animated heartBeat'
-                                })
-                                $('#contenido').attr('hidden', true)
-                                break;
-
-                            case 'bad':
-                                swal({
-                                    type: 'error',
-                                    title: "Error",
-                                    text: "Ha ocurrido un error al intentar guardar los datos",
-                                    button: "Aceptar",
-                                    icon: "error",
-                                    button: "Aceptar",
-                                    timer: 7000,
-                                    animation: false,
-                                    customClass: 'animated heartBeat'
-                                })
-                                break;
-                        }
-                    }
-                })
-            }
-        })
-    </script>
-    <script>
-        $(document).ready(function() {
-            $('#estado_paciente').on('change', function() {
-
-                if (this.value != '') {
-                    if (this.value == 'MUERTO') {
-                        $('div[name=fallecimiento]').attr('hidden', false)
-                        $('#programacion').attr('hidden', true)
-                        $('#error').attr('hidden', true)
-                    } else {
-                        $('div[name=fallecimiento]').attr('hidden', true)
-                        $('#programacion').attr('hidden', false)
-                        $('#error').attr('hidden', true)
-                    }
-                } else {
-                    $('#error').attr('hidden', false)
-                    $('div[name=fallecimiento]').attr('hidden', true)
-                    $('#programacion').attr('hidden', true)
-                }
-            })
-        })
-    </script>
-</body>
-
-</html>
+<?php require_once 'views/footer_view.php' ?>
